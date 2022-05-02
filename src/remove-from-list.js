2beb20de56dfc9c5620664731cca83f0674babd2
ArkadiiDavydenko <80512176+ArkadiiDavydenko@users.noreply.github.com>
2022-05-02 12:48:02 +0300

    complete: stack and remove

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,11 +22,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function removeKFromList(l, k ) {
+
+  let current = l;
+  while (current.next) {
+    if ((current.next.next === null)&&(current.next.value === k)) {
+      current.next = null
+      return l
+    }
+    if (current.value === k) {
+      current.value = current.next.value
+      current.next = current.next.next
+    }
+    else {
+      current = current.next
+    }
+  }
+  return l
 }
 
 module.exports = {
   removeKFromList
 };
+
